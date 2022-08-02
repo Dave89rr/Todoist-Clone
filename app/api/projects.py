e9b776a171fc17a28ddf94afe34cc73c7029ceff
Dave89rr <daleriro@gmail.com>
2022-08-02 18:30:40 -0400

    added create route for Projects

--- app/api/projects.py
+++ app/api/projects.py
@@ -1,5 +1,7 @@
-from flask import Blueprint
-from ..models import Project
+from turtle import color
+from flask import Blueprint, request
+from ..forms import ProjectForm
+from ..models import db, Project
 
 project = Blueprint("project", __name__, url_prefix='/api/projects')
 
@@ -10,3 +12,19 @@ def getEverything(ownerId):
     data = [proj.toDict() for proj in projects]
 
     return {'projects': data}
+
+@project.route('/create', methods=['POST'])
+def create():
+    form = ProjectForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        newProject = Project(
+            ownerId=form.data['ownerId'],
+            name=form.data['name'],
+            color=form.data['color'],
+            view=form.data['view']
+        )
+        db.session.add(newProject)
+        db.session.commit()
+        return newProject.toDict()
+    return 400
