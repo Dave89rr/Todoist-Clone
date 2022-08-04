a0cb709a7730353d0fd2210622429d74e9fdf6b1
Dave89rr <daleriro@gmail.com>
2022-08-04 09:20:54 -0400

    added backend validation for tasks

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, request
 from ..models import db, Task
+from ..forms import TaskForm
+from .auth_routes import validation_errors_to_error_messages
+
 
 task = Blueprint("task", __name__, url_prefix='/api/tasks')
 
@@ -13,27 +16,28 @@ def getEverything(ownerId):
 
 @task.route('/create', methods=['POST'])
 def create():
-    # form = TaskForm()
-    # form['csrf_token'].data = request.cookies['csrf_token']
-    # if form.validate_on_submit():
-    data = request.json
-    newtask = Task(
-        ownerId=data['ownerId'],
-        name=data['name'],
-        description=data['description'],
-        position=data['position'],
-        projectId=data['projectId'],
-        priority=data['priority'],
-        due_date=data['due_date'],
-        # ownerId=form.data['ownerId'],
-        # name=form.data['name'],
-        # color=form.data['color'],
-        # view=form.data['view']
-    )
-    db.session.add(newtask)
-    db.session.commit()
-    return newtask.toDict()
-    # return 400
+    form = TaskForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+    # data = request.json
+        newtask = Task(
+            ownerId=form.data['ownerId'],
+            name=form.data['name'],
+            description=form.data['description'],
+            position=form.data['position'],
+            projectId=form.data['projectId'],
+            priority=form.data['priority'],
+            due_date=form.data['due_date'],
+            # ownerId=form.data['ownerId'],
+            # name=form.data['name'],
+            # color=form.data['color'],
+            # view=form.data['view']
+        )
+        db.session.add(newtask)
+        db.session.commit()
+        return newtask.toDict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+
 
 
 @task.route('/update', methods=['PATCH'])
