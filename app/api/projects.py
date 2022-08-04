e59ececf5134c6862d254adc716d3f094fb7bf4f
Dave89rr <daleriro@gmail.com>
2022-08-04 09:04:47 -0400

    updating backend for validations

--- app/api/projects.py
+++ app/api/projects.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
 from ..forms import ProjectForm
 from ..models import db, Project
+from .auth_routes import validation_errors_to_error_messages
 
 project = Blueprint("project", __name__, url_prefix='/api/projects')
 
@@ -16,22 +17,17 @@ def getEverything(ownerId):
 def create():
     form = ProjectForm()
     form['csrf_token'].data = request.cookies['csrf_token']
-    # if form.validate_on_submit():
-    data = request.json
-    newProject = Project(
-        ownerId=data['ownerId'],
-        name=data['name'],
-        color=data['color'],
-        view=data['view']
-        # ownerId=form.data['ownerId'],
-        # name=form.data['name'],
-        # color=form.data['color'],
-        # view=form.data['view']
-    )
-    db.session.add(newProject)
-    db.session.commit()
-    return newProject.toDict()
-    # return 400
+    if form.validate_on_submit():
+        newProject = Project(
+            ownerId=form.data['ownerId'],
+            name=form.data['name'],
+            color=form.data['color'],
+            view=form.data['view']
+        )
+        db.session.add(newProject)
+        db.session.commit()
+        return newProject.toDict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 400
 
 
 @project.route('/update', methods=['PATCH'])
