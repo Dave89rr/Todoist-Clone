2496a977b7110e13a538bad4497eda2f9cbdc6ba
Dave89rr <daleriro@gmail.com>
2022-08-05 01:35:59 -0400

    added font theme switching to tasks

--- frontend/src/components/Elements/TaskView/TaskView.js
+++ frontend/src/components/Elements/TaskView/TaskView.js
@@ -1,15 +1,23 @@
+import classes from './TaskView.module.css';
 import EditTaskForm from '../../Forms/EditTaskForm';
 import { thunkDeleteTask } from '../../../store/tasks';
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 function TaskView({ task }) {
+  const user = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
   const [viewEditTask, setViewEditTask] = useState(false);
 
+  const theme = (name) => {
+    if (user) {
+      return `${classes[`${user.theme}${name}`]}`;
+    }
+  };
+
   return (
     <li key={task.id}>
-      {task.name}
+      <span className={`${theme('TaskTitle')}`}>{task.name}</span>
       <span>
         <button onClick={() => dispatch(thunkDeleteTask(task.id))}>
           Delete
