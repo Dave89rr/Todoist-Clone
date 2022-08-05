de5f4b7c6d788c24092d829211ceb9491d97f2c0
Dave89rr <daleriro@gmail.com>
2022-08-05 08:52:34 -0400

    added hover effect to edit buttons for tasks

--- frontend/src/components/Elements/TaskView/TaskView.js
+++ frontend/src/components/Elements/TaskView/TaskView.js
@@ -5,6 +5,8 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 function TaskView({ task }) {
+  const [showtaskUserInteractions, setShowtaskUserInteractions] =
+    useState(false);
   const user = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
   const [viewEditTask, setViewEditTask] = useState(false);
@@ -16,25 +18,31 @@ function TaskView({ task }) {
   };
 
   return (
-    <div key={task.id} className={classes.taskContainer}>
+    <div
+      key={task.id}
+      className={classes.taskContainer}
+      onMouseEnter={() => setShowtaskUserInteractions(true)}
+      onMouseLeave={() => setShowtaskUserInteractions(false)}
+    >
       <div className={classes.taskTitle}>
         <input type="checkbox"></input>
         <span className={`${theme('TaskTitle')}`}>{task.name}</span>
       </div>
       <div className={classes.taskUserInteractions}>
-        <span>
-          <button onClick={() => dispatch(thunkDeleteTask(task.id))}>
-            Delete
-          </button>
-          <button onClick={() => setViewEditTask(!viewEditTask)}>
-            Edit Task
-          </button>
-        </span>
+        {showtaskUserInteractions && (
+          <span>
+            <button onClick={() => dispatch(thunkDeleteTask(task.id))}>
+              Delete
+            </button>
+            <button onClick={() => setViewEditTask(!viewEditTask)}>
+              Edit Task
+            </button>
+          </span>
+        )}
       </div>
       {viewEditTask && (
         <div className={classes.editForm}>
-          {' '}
-          <EditTaskForm taskProp={task} />{' '}
+          <EditTaskForm taskProp={task} />
         </div>
       )}
     </div>
