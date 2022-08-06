5b9f69221703af9d1d04d1d3e61bd3ba668f901b
Dave89rr <daleriro@gmail.com>
2022-08-06 14:17:43 -0400

    updated today page to have header match theme

--- frontend/src/components/Pages/TodayPage/TodayPage.js
+++ frontend/src/components/Pages/TodayPage/TodayPage.js
@@ -4,16 +4,23 @@ import TaskView from '../../Elements/TaskView/TaskView';
 
 function TodayPage() {
   // const projects = useSelector((state) => state.projects);
+  const user = useSelector((state) => state.session.user);
   const tasks = useSelector((state) => state.tasks);
   let taskArr;
   if (tasks) {
     taskArr = Object.values(tasks);
   }
+  const theme = (name) => {
+    if (user) {
+      return `${user.theme}${name}`;
+    }
+  };
   return (
     <div className={classes.mainContainer}>
       <div className={classes.projectContainer}>
+        <h1 className={classes[`${theme('SectionTitle')}`]}>Overdue</h1>
         {taskArr.map((task) => {
-          if (new Date() < new Date(task.due_date)) {
+          if (new Date() > new Date(task.due_date)) {
             return <TaskView task={task} key={task.id} />;
           }
           return null;
