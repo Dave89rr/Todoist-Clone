4dbcd4d5067151c77e70b7b69e2158b76119b196
David Rivera <daleriro@gmail.com>
2022-08-08 08:40:07 -0400

    added animation to edit project form modal

--- frontend/src/components/Elements/ProjectView/ProjectView.js
+++ frontend/src/components/Elements/ProjectView/ProjectView.js
@@ -6,6 +6,7 @@ import { thunkDeleteProject } from '../../../store/projects';
 import { useState } from 'react';
 import TaskView from '../TaskView/TaskView';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import { CSSTransition } from 'react-transition-group';
 
 function ProjectView() {
   const { projectId } = useParams();
@@ -28,12 +29,21 @@ function ProjectView() {
   return (
     <div className={classes.mainContainer}>
       <div className={classes.projectContainer}>
-        {viewEditProject && (
+        <CSSTransition
+          in={viewEditProject}
+          classNames={{
+            enterActive: classes.editFormEnterActive,
+            enterDone: classes.editFormEnter,
+            exitActive: classes.editFormExitActive,
+            exit: classes.editFormExit,
+          }}
+          unmountOnExit
+        >
           <EditProjectForm
             projectProp={project}
             setViewEditProject={setViewEditProject}
           />
-        )}
+        </CSSTransition>
         <div key={project.id} className={classes.projContainer}>
           <div className={classes.projTitle}>
             <span className={`${theme('ProjHeader')}`}>{project.name}</span>
