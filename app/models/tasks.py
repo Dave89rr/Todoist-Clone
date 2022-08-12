ec4d091e236e9468ec80814b191a4030b5b5d7fe
David Rivera <daleriro@gmail.com>
2022-08-12 17:02:14 -0400

    fixed tasks table in db

--- app/models/tasks.py
+++ app/models/tasks.py
@@ -11,7 +11,8 @@ class Task(db.Model):
     position = db.Column(db.Integer, nullable=False)
     projectId = db.Column(db.Integer, db.ForeignKey("projects.id", ondelete='CASCADE'), nullable=False)
     priority = db.Column(db.Integer, nullable=False)
-    due_date = db.Column(db.DateTime, nullable=True)
+    due_date = db.Column(db.DateTime(timezone=True), nullable=True)
+    completed = db.Column(db.Boolean, nullable=False)
     createdAt = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now())
     updatedAt = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
 
@@ -26,6 +27,7 @@ class Task(db.Model):
             projectId=self.projectId,
             priority=self.priority,
             due_date=self.due_date,
+            completed=self.completed,
             createdAt=self.createdAt,
             updatedAt=self.updatedAt,
         )
