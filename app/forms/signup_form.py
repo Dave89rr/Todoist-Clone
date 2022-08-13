59bc669b96105e009e8e02971ed5c752634631e2
David Rivera <daleriro@gmail.com>
2022-08-13 18:01:17 -0400

    added email validations (fix #30)

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -23,7 +23,7 @@ def username_exists(form, field):
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[Length(min=1, max=12, message='Username must be between 1 and 12 characters long'), username_exists])
-    email = StringField('email', validators=[Length(min=1, max=40, message='Email must be valid and less than 40 characters long.'), user_exists])
+    email = StringField('email', validators=[Length(min=1, max=40, message='Email must be valid and less than 40 characters long.'), user_exists, Email(check_deliverability=True, granular_message=True)])
     password = StringField('password', validators=[Length(min=8, max=60, message='Password must be between 8 and 60 characters long.')])
     icon_url = StringField('icon_url', validators=[DataRequired()])
     theme = BooleanField('Theme')
