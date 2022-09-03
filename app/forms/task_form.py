19bee977952f36ccdc02ec9f49e26c1dc1e0482f
David Rivera <daleriro@gmail.com>
2022-09-03 14:35:26 -0400

    updated validators for new task name length

--- app/forms/task_form.py
+++ app/forms/task_form.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Length
 
 
 class TaskForm(FlaskForm):
-    name = StringField('name', validators=[Length(min=1, max=30, message='Name must be between 1 and 30 characters long.')])
+    name = StringField('name', validators=[Length(min=1, max=500, message='Name must be between 1 and 500 characters long.')])
     ownerId = IntegerField('ownerId', validators=[DataRequired(), ])
     description = StringField('description', validators=[Length(min=0, max=2000, message='Description must be less than 2000 characters long.')])
     position = IntegerField('position', validators=[DataRequired()])
--- app/forms/update_task_form.py
+++ app/forms/update_task_form.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired, Length
 
 class UpdateTaskForm(FlaskForm):
     id = IntegerField('id', validators=[DataRequired()])
-    name = StringField('name', validators=[Length(min=1, max=30, message='Name must be between 1 and 30 characters long.')])
+    name = StringField('name', validators=[Length(min=1, max=500, message='Name must be between 1 and 500 characters long.')])
     ownerId = IntegerField('ownerId', validators=[DataRequired()])
     description = StringField('description', validators=[Length(min=0, max=2000, message='Description must be less than 2000 characters long.')])
     position = IntegerField('position', validators=[DataRequired()])
